1117311bd704c7672ee6b0b3fed4aadda08fff0c
aaronhaoranshang <48455186+aaronhaoranshang@users.noreply.github.com>
2025-03-30 22:09:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("bookBtn").onclick = function () {
-  window.open("https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec", "_blank");
+  window.open("https://script.google.com/macros/s/AKfycbxEtNrAYpba5MRgsNi7s3EfQjycMwMj90n7h_iDO3iexKaANBT7pj0_88tk1GQv7QwGZw/exec", "_blank");
 };
