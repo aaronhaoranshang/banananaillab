76fbe6db083f3a8ee0d649674955fa0187699f20
aaronhaoranshang <48455186+aaronhaoranshang@users.noreply.github.com>
2025-03-30 22:51:06 -0400

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("bookBtn").addEventListener("click", function () {
-  window.open("https://script.google.com/macros/s/AKfycbyXF8OrjITIBLy5R71_2fNz_PqzmHerPo37XqmNFTaQgbKYCpB-XCs5na4ZFV3mMvvIww/exec", "_blank");
-});
